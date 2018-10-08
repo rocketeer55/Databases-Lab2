fb1c82cd1dfe510d4c4ed1e98cb958fb6eb6705a
Spencer Schurk <spencer@joyrun.com>
2018-10-08 13:58:16 -0700

    Added cars test

--- CARS-test.sql
+++ CARS-test.sql
@@ -1,20 +1,20 @@
 -- Jack Goyette, Spencer Schurk
 -- jgoyette@calpoly.edu, sschurk@calpoly.edu
 
-SELECT * FROM CARMAKERS;
-SELECT COUNT(*) FROM CARMAKERS;
+SELECT * FROM CARMAKERS; -- should print table with all data from car-makers.csv
+SELECT COUNT(*) FROM CARMAKERS; -- should display 23 tuples
 
-SELECT * FROM CARNAMES;
-SELECT COUNT(*) FROM CARNAMES;
+SELECT * FROM CARNAMES; -- should print table with all data from car-names.csv
+SELECT COUNT(*) FROM CARNAMES; -- should display 406 tuples
 
-SELECT * FROM CARSDATA;
-SELECT COUNT(*) FROM CARSDATA;
+SELECT * FROM CARSDATA; -- should print table with all data from cars-data.csv
+SELECT COUNT(*) FROM CARSDATA; -- should display 406 tuples
 
-SELECT * FROM CONTINENTS;
-SELECT COUNT(*) FROM CONTINENTS;
+SELECT * FROM CONTINENTS; -- should print table with all data from continents.csv
+SELECT COUNT(*) FROM CONTINENTS; -- should display 5 tuples
 
-SELECT * FROM COUNTRIES;
-SELECT COUNT(*) FROM COUNTRIES;
+SELECT * FROM COUNTRIES; -- should print table with all data from countries.csv
+SELECT COUNT(*) FROM COUNTRIES; -- should display 15 tuples
 
-SELECT * FROM MODELLIST;
-SELECT COUNT(*) FROM MODELLIST;
+SELECT * FROM MODELLIST; -- should print table with all data from model-list.csv
+SELECT COUNT(*) FROM MODELLIST; -- should display 36 tuples
